95c40cf1f8d2ffb126b97fb1737fc5bf637cedac
Akash Ramesh <75295977+Akash-john@users.noreply.github.com>
2022-06-02 05:36:16 +0530

    Type conversion

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-import "./styles.css";
+// let value = true;
+// console.log(typeof value)
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// value = String(value);
+// console.log(typeof value)
+
+//Number conversion
+
+// let str = "123"
+
+// console.log(typeof str)
+
+// str = Number(str)
+
+// console.log(typeof str)
+
+// let age =Number("Hello")
+
+// console.log(age)
+
+//numberic conversion rules
+//undefined = Nan
+//null = 0
+////true or false ,1,0
+//"" = 0 ,other wise Nan.
+
+//Boolean Conversion
+//undefined,null,NaN
+
+let checkVar = "";
+
+checkVar = Number(checkVar);
+console.log(checkVar);
